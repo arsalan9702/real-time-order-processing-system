523d28bf27b860f693e284f348f25af3a13816f6
arsalan <arsalansayed9702@gmail.com>
2025-04-06 13:11:11 +0530

    made consumer idk

--- consumer/kafka.go
+++ consumer/kafka.go
@@ -1,1 +1,48 @@
-package consumer
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"real-time-order-processing-system/config"
+	"real-time-order-processing-system/models"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+func StartKafkaConsumer() {
+	consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": "localhost:9092",
+		"group.id": "order-consumer-group",
+		"auto.offset.reset": "earliest",
+	})
+	if err != nil {
+		log.Fatal("Failed to create kafka consumer:", err)
+	}
+
+	err = consumer.Subscribe("orders", nil)
+	if err != nil {
+		log.Fatal("Failed to subscribe to topic orders:",err)
+	}
+	fmt.Println("Kafka consumer started, listening on orders...")
+
+	for{
+		message, err := consumer.ReadMessage(-1)
+		if err != nil {
+			log.Println("Consumer error:",err)
+			continue	
+		}
+
+		var order models.Order
+		if err := json.Unmarshal(message.Value, &order); err != nil {
+			log.Println("Failed to parse order JSON:",err)
+			continue
+		} 
+
+		if err := config.DB.Model(&order).Where("id = ?", order.ID).Update("status", "Processed").Error; err != nil{
+			log.Println("Failed to update order in DB:", err)
+		} else {
+			fmt.Printf("Order ID %d processed\n", order.ID)
+		}
+	}
+}
--- consumer/main.go
+++ consumer/main.go
@@ -1,1 +1,13 @@
-package consumer
+package main
+
+import (
+	"fmt"
+	"real-time-order-processing-system/config"
+)
+
+func main() {
+	config.InitDB()
+
+	fmt.Println("Consumer service starting")
+	StartKafkaConsumer()
+}
